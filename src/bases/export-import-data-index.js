43f516db6ebbdbf6202ecb76114df449561e4d47
ewi-orion <brpmdr@mailfence.com>
2022-06-14 21:09:27 -0400

    import - export

---
+++ src/bases/export-import-data-index.js
@@ -0,0 +1,9 @@
+console.log('Hola!!')
+
+import { getHeroById, getHeroesByOwner } from './bases/import-export'
+
+getHeroById
+console.log(getHeroById(2))
+
+getHeroesByOwner
+console.log(getHeroesByOwner('DC'))

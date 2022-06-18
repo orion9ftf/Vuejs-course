7e5f25e863667669cff9f70c7501d8c58fccf227
ewi-orion <brpmdr@mailfence.com>
2022-06-18 14:17:30 -0400

    async and await exercices

---
+++ src/bases/async_await_aplicado.js
@@ -0,0 +1,20 @@
+console.log('Hola!!')
+import giphyApi from "./bases/axios"
+
+const getImage = async () => {
+  try {
+    const { data } = (await giphyApi.get('/random')).data
+    const { url } = data.images.original
+
+    const img = document.createElement('img')
+    img.src = url
+    document.body.append(img)
+
+  
+  } catch (error) {
+    console.log('Error en esta petición', error)
+    throw error
+  }
+}
+
+getImage()

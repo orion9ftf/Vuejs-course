5eaad5476772443f3f29ccfb0ece7bf01148e98c
ewi-orion <brpmdr@mailfence.com>
2022-06-17 21:08:07 -0400

    axios API

---
+++ src/bases/axios.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+const apiKey = API_KEY
+// https://api.giphy.com/v1/gifs/random?api_key=
+
+// fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+//   .then(resp => resp.json())
+//   .then(({ data }) => {
+//       const { url } = data.images.original
+
+//       const img = document.createElement('img')
+//       img.src = url
+
+//       document.body.append( img )
+//   })
+
+  // sin axios
+
+  const giphyApi = axios.create({
+    baseURL: 'https://api.giphy.com/v1/gifs',
+    params: {
+      api_key: apiKey
+    }
+  })
+
+  giphyApi.get('/random')
+    .then(resp => {
+      //console.log(resp.data.data.images.original.url)
+
+      //extraer url
+      const { data } = resp.data
+      const { url } = data.images.original
+
+      console.log(data)
+      console.log('la url con destructuración', url)
+
+      const img = document.createElement('img')
+      img.src = url 
+
+      document.body.append(img)
+    })
+
+
